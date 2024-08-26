e35d0cf9d2d482cc66916c5b51ca0bcff774fe75
leoleonsoles <leonsoles6@hotmail.com>
2024-08-25 23:05:14 -0500

    Update Proyecto_DDP.java
    borrado

--- src/proyecto_ddp/Proyecto_DDP.java
+++ src/proyecto_ddp/Proyecto_DDP.java
@@ -1,23 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto_ddp;
-
-/**
- *
- * @author roman
- */
 public class Proyecto_DDP {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-        //svadfad
-        //Pablo Casas
-        //afkasfln
     }
     
 }
